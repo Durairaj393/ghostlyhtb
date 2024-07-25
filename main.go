59d1b87ebf4778b749ebdb5a1c2d1f32459aaa7c
Durairaj393 <140406577+Durairaj393@users.noreply.github.com>
2024-07-25 09:31:06 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,77 +1,199 @@
 package main
 
 import (
-    "html/template"
-    "net/http"
-    "os"
-    "os/exec"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
+const WEB_PORT = "1337"
+const TEMPLATE_DIR = "./templates"
+
 type LocationInfo struct {
-    Country  string
-    Region   string
-    City     string
-    ISP      string
+	Status      string  `json:"status"`
+	Country     string  `json:"country"`
+	CountryCode string  `json:"countryCode"`
+	Region      string  `json:"region"`
+	RegionName  string  `json:"regionName"`
+	City        string  `json:"city"`
+	Zip         string  `json:"zip"`
+	Lat         float64 `json:"lat"`
+	Lon         float64 `json:"lon"`
+	Timezone    string  `json:"timezone"`
+	ISP         string  `json:"isp"`
+	Org         string  `json:"org"`
+	AS          string  `json:"as"`
+	Query       string  `json:"query"`
 }
 
 type MachineInfo struct {
-    Hostname      string
-    OS            string
-    KernelVersion string
-    Memory        string
+	Hostname      string
+	OS            string
+	KernelVersion string
+	Memory        string
 }
 
 type RequestData struct {
-    Title        string
-    Message      string
-    ClientIP     string
-    ClientUA     string
-    ServerInfo   MachineInfo
-    ClientIpInfo LocationInfo
+	ClientIP     string
+	ClientUA     string
+	ServerInfo   MachineInfo
+	ClientIpInfo LocationInfo `json:"location"`
 }
 
 func GetServerInfo(command string) string {
-    out, err := exec.Command("sh", "-c", command).Output()
-    if err != nil {
-        return ""
-    }
-    return string(out)
+	out, err := exec.Command("sh", "-c", command).Output()
+	if err != nil {
+		return ""
+	}
+	return string(out)
+}
+
+func (p RequestData) GetLocationInfo(endpointURL string) (*LocationInfo, error) {
+	resp, err := http.Get(endpointURL)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("HTTP request failed with status code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var locationInfo LocationInfo
+	if err := json.Unmarshal(body, &locationInfo); err != nil {
+		return nil, err
+	}
+
+	return &locationInfo, nil
+}
+
+func (p RequestData) IsSubdirectory(basePath, path string) bool {
+	rel, err := filepath.Rel(basePath, path)
+	if err != nil {
+		return false
+	}
+	return !strings.HasPrefix(rel, ".."+string(filepath.Separator))
+}
+
+func (p RequestData) OutFileContents(filePath string) string {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return err.Error()
+	}
+	return string(data)
+}
+
+func readRemoteFile(url string) (string, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("HTTP request failed with status code: %d", response.StatusCode)
+	}
+
+	content, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
+}
+
+func getIndex(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view?page=index.tpl", http.StatusMovedPermanently)
 }
 
 func getTpl(w http.ResponseWriter, r *http.Request) {
-    reqData := &RequestData{
-        Title:    "Welcome",
-        Message:  "Hello, World!",
-        ClientIP: r.RemoteAddr,
-        ClientUA: r.Header.Get("User-Agent"),
-        ServerInfo: MachineInfo{
-            Hostname:      GetServerInfo("hostname"),
-            OS:            GetServerInfo("cat /etc/os-release | grep PRETTY_NAME | cut -d '\"' -f 2"),
-            KernelVersion: GetServerInfo("uname -r"),
-            Memory:        GetServerInfo("free -h | awk '/^Mem/{print $2}'"),
-        },
-        ClientIpInfo: LocationInfo{
-            Country: "Unknown",
-            Region:  "Unknown",
-            City:    "Unknown",
-            ISP:     "Unknown",
-        },
-    }
+	var page string = r.URL.Query().Get("page")
+	var remote string = r.URL.Query().Get("remote")
 
-    tmpl, err := template.ParseFiles("templates/index.tpl")
-    if err != nil {
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-        return
-    }
+	if page == "" {
+		http.Error(w, "Missing required parameters", http.StatusBadRequest)
+		return
+	}
 
-    err = tmpl.Execute(w, reqData)
-    if err != nil {
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-        return
-    }
+	reqData := &RequestData{}
+
+	userIPCookie, err := r.Cookie("user_ip")
+	clientIP := ""
+
+	if err == nil {
+		clientIP = userIPCookie.Value
+	} else {
+		clientIP = strings.Split(r.RemoteAddr, ":")[0]
+	}
+
+	userAgent := r.Header.Get("User-Agent")
+
+	locationInfo, err := reqData.GetLocationInfo("https://freeipapi.com/api/json/" + clientIP)
+
+	if err != nil {
+		http.Error(w, "Could not fetch IP location info", http.StatusInternalServerError)
+		return
+	}
+
+	reqData.ClientIP = clientIP
+	reqData.ClientUA = userAgent
+	reqData.ClientIpInfo = *locationInfo
+	reqData.ServerInfo.Hostname = GetServerInfo("hostname")
+	reqData.ServerInfo.OS = GetServerInfo("cat /etc/os-release | grep PRETTY_NAME | cut -d '\"' -f 2")
+	reqData.ServerInfo.KernelVersion = GetServerInfo("uname -r")
+	reqData.ServerInfo.Memory = GetServerInfo("free -h | awk '/^Mem/{print $2}'")
+
+	var tmplFile string
+
+	if remote == "true" {
+		tmplFile, err = readRemoteFile(page)
+
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	} else {
+		if !reqData.IsSubdirectory("./", TEMPLATE_DIR+"/"+page) {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		tmplFile = reqData.OutFileContents(TEMPLATE_DIR + "/" + page)
+	}
+
+	tmpl, err := template.New("page").Parse(tmplFile)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, reqData)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-    http.HandleFunc("/view", getTpl)
-    http.ListenAndServe(":1337", nil)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", getIndex)
+	mux.HandleFunc("/view", getTpl)
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	fmt.Println("Server started at port " + WEB_PORT)
+	http.ListenAndServe(":"+WEB_PORT, mux)
 }
